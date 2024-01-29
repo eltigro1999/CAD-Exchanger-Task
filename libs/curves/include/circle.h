2d8d86bbc2d1dcd88620b20bf81bc9832f86237c
El Tigro <eltigro1999@gmail.com>
2024-01-30 00:08:48 +0300

    Completed the tasks

--- libs/curves/include/circle.h
+++ libs/curves/include/circle.h
@@ -9,11 +9,13 @@ class Circle : public Curve
 public:
     Circle();
     Circle(const double &xCenter, const double &yCenter, const double &radius);
-    Circle(const Ellipse &ellipse);
+    Circle(const Circle &Circle);
     Point get3DPoint(const double &t) const override;
     Vector get3DVector(const double &t) const override;
+    const double getRadius();
     virtual ~Circle();
 
 private:
     Ellipse m_ellipse;
+    double m_radius;
 };
--- libs/curves/src/circle.cpp
+++ libs/curves/src/circle.cpp
@@ -1,18 +1,18 @@
 #include "circle.h"
 
-Circle::Circle() : m_ellipse(0, 0, 1, 1)
+Circle::Circle() : Circle(0, 0, 1)
 {
 }
 Circle::Circle(const double &xCenter, const double &yCenter, const double &radius) : m_ellipse(xCenter,
                                                                                                yCenter,
                                                                                                radius,
-                                                                                               radius)
+                                                                                               radius),
+                                                                                     m_radius(radius)
 {
 }
 
-Circle::Circle(const Ellipse &ellipse)
+Circle::Circle(const Circle &circle) : m_ellipse(circle.m_ellipse), m_radius(circle.m_radius)
 {
-    m_ellipse = ellipse;
 }
 
 Point Circle::get3DPoint(const double &t) const
@@ -23,6 +23,11 @@ Point Circle::get3DPoint(const double &t) const
 Vector Circle::get3DVector(const double &t) const
 {
     return m_ellipse.get3DVector(t);
+}
+
+const double Circle::getRadius()
+{
+    return m_radius;
 }
 
 Circle::~Circle() {}
